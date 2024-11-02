c6348da0ca1a1b61b783df6a04d3e81f3b5ef07e
Viswajith2003 <viswajithviswa715@gmail.com>
2024-11-02 23:57:17 +0530

    word char line modified

--- LEX/lex.yy.c
+++ LEX/lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 4
-#define YY_END_OF_BUFFER 5
+#define YY_NUM_RULES 5
+#define YY_END_OF_BUFFER 6
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,27 +360,27 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[9] =
+static const flex_int16_t yy_accept[10] =
     {   0,
-        0,    0,    5,    3,    4,    1,    2,    0
+        0,    0,    6,    3,    1,    2,    4,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
+        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    2,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    3,    4,    4,    4,    3,    4,
-        4,    4,    3,    4,    4,    4,    4,    4,    3,    4,
-        4,    4,    4,    4,    3,    4,    4,    4,    4,    4,
-        1,    1,    1,    1,    1,    1,    3,    4,    4,    4,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
 
-        3,    4,    4,    4,    3,    4,    4,    4,    4,    4,
-        3,    4,    4,    4,    4,    4,    3,    4,    4,    4,
-        4,    4,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -397,29 +397,31 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[5] =
+static const YY_CHAR yy_meta[4] =
     {   0,
-        1,    1,    1,    1
+        1,    2,    3
     } ;
 
-static const flex_int16_t yy_base[9] =
+static const flex_int16_t yy_base[12] =
     {   0,
-        0,    0,    5,    6,    6,    6,    6,    6
+        0,    0,    6,    0,    0,    7,    0,    0,    7,    4,
+        2
     } ;
 
-static const flex_int16_t yy_def[9] =
+static const flex_int16_t yy_def[12] =
     {   0,
-        8,    1,    8,    8,    8,    8,    8,    0
+        9,    1,    9,   10,   11,    9,   10,   11,    0,    9,
+        9
     } ;
 
 static const flex_int16_t yy_nxt[11] =
     {   0,
-        4,    5,    6,    7,    8,    3,    8,    8,    8,    8
+        4,    5,    6,    8,    7,    9,    3,    9,    9,    9
     } ;
 
 static const flex_int16_t yy_chk[11] =
     {   0,
-        1,    1,    1,    1,    3,    8,    8,    8,    8,    8
+        1,    1,    1,   11,   10,    3,    9,    9,    9,    9
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -436,12 +438,11 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "vowels.l"
-#line 2 "vowels.l"
-int v = 0, c = 0;
-char vow[100] = "", cons[100] = "";
-#line 444 "lex.yy.c"
+#line 1 "WrdLineChar.l"
+#line 2 "WrdLineChar.l"
+    int wc = 0, lc = 0, cc = 0;
 #line 445 "lex.yy.c"
+#line 446 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -658,9 +659,10 @@ YY_DECL
 		}
 
 	{
-#line 9 "vowels.l"
+#line 5 "WrdLineChar.l"
 
-#line 664 "lex.yy.c"
+
+#line 666 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -687,13 +689,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 9 )
+				if ( yy_current_state >= 10 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 6 );
+		while ( yy_base[yy_current_state] != 7 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -719,25 +721,31 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 10 "vowels.l"
-{ strcat(vow, yytext); v++; }
+#line 7 "WrdLineChar.l"
+;              // Ignore spaces and tabs
 	YY_BREAK
 case 2:
+/* rule 2 can match eol */
 YY_RULE_SETUP
-#line 11 "vowels.l"
-{ strcat(cons, yytext); c++; }
+#line 8 "WrdLineChar.l"
+{ lc++; }      // Newline increments line count
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 12 "vowels.l"
-{}
+#line 9 "WrdLineChar.l"
+{ cc++; }      // Any character increments character count
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 13 "vowels.l"
+#line 10 "WrdLineChar.l"
+{ wc++; }      // Sequence of non-space, non-newline characters is treated as a word
+	YY_BREAK
+case 5:
+YY_RULE_SETUP
+#line 12 "WrdLineChar.l"
 ECHO;
 	YY_BREAK
-#line 741 "lex.yy.c"
+#line 749 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1034,7 +1042,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 9 )
+			if ( yy_current_state >= 10 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1062,11 +1070,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 9 )
+		if ( yy_current_state >= 10 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 8);
+	yy_is_jam = (yy_current_state == 9);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1742,28 +1750,24 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 13 "vowels.l"
+#line 12 "WrdLineChar.l"
 
 
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 int yywrap(void) { return 1; }
 
 void main() {
-    // char input[100];
-    // printf("\nEnter the String: ");
-    // fgets(input, 100, stdin);
+    yyin = fopen("./textFiles/text.txt", "r");
+    if (!yyin) {
+        perror("Error opening file");
+        exit(1);
+    }
 
-    // Set yyin to read from the input string
-    // yy_scan_string(input);
+    yylex();
+    fclose(yyin);
 
-    yyin=fopen("./textFiles/vowel.txt","r");
-    yylex();  // Process the input
-    
-    printf("\nVowel count: %d", v);
-    printf("\nConsonant count: %d", c);
-    printf("\nVowels are: %s", vow);
-    printf("\nConsonants are: %s", cons);
+    printf("\nWord count: %d\nLine count: %d\nCharacter count: %d\n", wc, lc, cc);
 }
 
