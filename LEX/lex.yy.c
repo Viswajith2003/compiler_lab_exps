33812648b779abcef41dab733eb0ad29b983bb0b
Viswajith2003 <viswajithviswa715@gmail.com>
2024-11-03 09:42:00 +0530

    strip cmds set

--- LEX/lex.yy.c
+++ LEX/lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 5
-#define YY_END_OF_BUFFER 6
+#define YY_NUM_RULES 3
+#define YY_END_OF_BUFFER 4
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,18 +360,19 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[10] =
+static const flex_int16_t yy_accept[13] =
     {   0,
-        0,    0,    6,    3,    1,    2,    4,    1,    0
+        0,    0,    4,    3,    3,    0,    1,    0,    0,    1,
+        2,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    2,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    3,    1,    1,    1,    1,    4,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -397,31 +398,33 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[4] =
+static const YY_CHAR yy_meta[5] =
     {   0,
-        1,    2,    3
+        1,    2,    1,    1
     } ;
 
-static const flex_int16_t yy_base[12] =
+static const flex_int16_t yy_base[15] =
     {   0,
-        0,    0,    6,    0,    0,    7,    0,    0,    7,    4,
-        2
+       10,    9,   12,   15,    0,    4,    0,    0,    7,    0,
+        0,   15,    8,    0
     } ;
 
-static const flex_int16_t yy_def[12] =
+static const flex_int16_t yy_def[15] =
     {   0,
-        9,    1,    9,   10,   11,    9,   10,   11,    0,    9,
-        9
+       13,   13,   12,   12,   12,   12,   14,    6,    6,   14,
+        6,    0,   12,   12
     } ;
 
-static const flex_int16_t yy_nxt[11] =
+static const flex_int16_t yy_nxt[20] =
     {   0,
-        4,    5,    6,    8,    7,    9,    3,    9,    9,    9
+       10,   12,    6,    7,    8,    8,    9,    8,    4,    4,
+       11,   12,    5,    5,    3,   12,   12,   12,   12
     } ;
 
-static const flex_int16_t yy_chk[11] =
+static const flex_int16_t yy_chk[20] =
     {   0,
-        1,    1,    1,   11,   10,    3,    9,    9,    9,    9
+       14,    0,    5,    5,    6,    6,    6,    6,   13,   13,
+        9,    3,    2,    1,   12,   12,   12,   12,   12
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -438,11 +441,11 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "WrdLineChar.l"
-#line 2 "WrdLineChar.l"
-    int wc = 0, lc = 0, cc = 0;
-#line 445 "lex.yy.c"
-#line 446 "lex.yy.c"
+#line 1 "stripCmds.l"
+#line 2 "stripCmds.l"
+#include<stdio.h>
+#line 448 "lex.yy.c"
+#line 449 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -659,10 +662,9 @@ YY_DECL
 		}
 
 	{
-#line 5 "WrdLineChar.l"
+#line 4 "stripCmds.l"
 
-
-#line 666 "lex.yy.c"
+#line 668 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -689,13 +691,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 10 )
+				if ( yy_current_state >= 13 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 7 );
+		while ( yy_base[yy_current_state] != 15 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -721,31 +723,21 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 7 "WrdLineChar.l"
-;              // Ignore spaces and tabs
+#line 5 "stripCmds.l"
+{}
 	YY_BREAK
 case 2:
 /* rule 2 can match eol */
 YY_RULE_SETUP
-#line 8 "WrdLineChar.l"
-{ lc++; }      // Newline increments line count
+#line 6 "stripCmds.l"
+{}
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 9 "WrdLineChar.l"
-{ cc++; }      // Any character increments character count
-	YY_BREAK
-case 4:
-YY_RULE_SETUP
-#line 10 "WrdLineChar.l"
-{ wc++; }      // Sequence of non-space, non-newline characters is treated as a word
-	YY_BREAK
-case 5:
-YY_RULE_SETUP
-#line 12 "WrdLineChar.l"
+#line 7 "stripCmds.l"
 ECHO;
 	YY_BREAK
-#line 749 "lex.yy.c"
+#line 741 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1042,7 +1034,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 10 )
+			if ( yy_current_state >= 13 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1070,11 +1062,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 10 )
+		if ( yy_current_state >= 13 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 9);
+	yy_is_jam = (yy_current_state == 12);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1750,19 +1742,12 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 12 "WrdLineChar.l"
+#line 7 "stripCmds.l"
 
-
-#include <stdio.h>
-#include <stdlib.h>
-
-int yywrap(void) { return 1; }
-
-void main() {
-    yyin = fopen("./textFiles/text.txt", "r");
-
+int yywrap(void) {}
+void main()
+{
+    yyin=fopen("textFiles/input.c","r");
+    yyout=fopen("textFiles/output.c","w");
     yylex();
-
-    printf("\nWord count: %d\nLine count: %d\nCharacter count: %d\n", wc, lc, cc);
 }
-
---
+++ LEX/textFiles/input.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+// This is single line commentint main() {
+/*This is
+multi line comment*/
+printf("Hello World \n");
+return 0;
+}
